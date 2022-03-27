b3e5621cd6124d981c106a20c5fb1f58cce6bf38
Saikiran Vanama <saikiran.vanama@student.csulb.edu>
2022-03-26 22:59:05 -0700

    React Hooks Impl

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Accordion from "./components/Accordion";
+const  items = [
+    {
+        title: 'What is React ?',
+        content: 'React is a JS frame work'
+    },
+    {
+        title: 'Why use React ?',
+        content: 'React is a favorite front-end JS frame work'
+    },
+    {
+        title: 'How do you use React ?',
+        content: 'You use React by creating components'
+    }
+]
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Accordion items={items}/>
+        </div>
+    );
 }
 
 export default App;
